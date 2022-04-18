53060f0e4e8a48b44c097c6b5bed2c8b87a80edb
jdkato <joseph@jdkato.io>
2022-04-17 18:11:56 -0700

    WIP: copy

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -2,8 +2,7 @@ import Clipboard from 'clipboard';
 
 var pre = document.getElementsByTagName('pre');
 
-for (var i = 0; i < pre.length; ++ i)
-{
+for (var i = 0; i < pre.length; ++i) {
   var element = pre[i];
   var mermaid = element.getElementsByClassName('language-mermaid')[0];
 
@@ -14,24 +13,23 @@ for (var i = 0; i < pre.length; ++ i)
 
 var clipboard = new Clipboard('.btn-copy', {
 
-  target: function(trigger) {
+  target: function (trigger) {
     return trigger.nextElementSibling;
   },
 
 });
 
-clipboard.on('success', function(e) {
+clipboard.on('success', function (e) {
 
-    /*
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-    */
+  /*
+  console.info('Action:', e.action);
+  console.info('Text:', e.text);
+  console.info('Trigger:', e.trigger);
+  */
 
-    e.clearSelection();
+  e.clearSelection();
 });
 
-clipboard.on('error', function(e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
+clipboard.on('error', function (e) {
+  console.error('Action:', e);
 });
