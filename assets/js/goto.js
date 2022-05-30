54c42685b639e6326657e0d2cab26ca1dcb7b8aa
jdkato <joseph@jdkato.io>
2022-05-30 08:09:50 -0700

    Typo

--- assets/js/goto.js
+++ assets/js/goto.js
@@ -1,5 +1,6 @@
 import { autocomplete } from '@algolia/autocomplete-js';
-import {parse} from 'query-string';
+import { parse } from 'query-string';
+import { getLinkPreview } from 'link-preview-js';
 
 autocomplete({
   container: '#autocomplete',
@@ -23,17 +24,20 @@ autocomplete({
               },
               item({ item, html }) {
                 const parsed = parse(item.ID);
-                return html`<div class="card text-muted">
-                  <div class="card-body">
-                    <h5 class="card-title mt-0">${parsed.title}</h5>
-                    <h6 class="card-subtitle pb-2">
-                      <span class="badge rounded-pill bg-secondary result-tag"><i class="fas fa-calendar"></i> ${parsed.year}</span>
-                      <span class="badge rounded-pill bg-secondary result-tag"><i class="fas fa-user"></i> ${parsed.author}</span>
-                    </h6>
-                    <p class="card-text">${item.Fragment}</p>
-                    <a href="${parsed.url}" class="stretched-link" target="_blank"></a>
-                  </div>
-                </div>`
+                return getLinkPreview(parsed.url).then((preview) => {
+                  return html`<div class="card text-muted">
+                    <img class="card-img-top" src="${preview.images[0]}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title mt-0">${parsed.title}</h5>
+                        <h6 class="card-subtitle pb-2">
+                        <span class="badge rounded-pill bg-secondary result-tag"><i class="fas fa-calendar"></i> ${parsed.year}</span>
+                        <span class="badge rounded-pill bg-secondary result-tag"><i class="fas fa-user"></i> ${parsed.author}</span>
+                        </h6>
+                        <p class="card-text">${item.Fragment}</p>
+                        <a href="${parsed.url}" class="stretched-link" target="_blank"></a>
+                    </div>
+                  </div>`
+                });
               },
             },
           },
