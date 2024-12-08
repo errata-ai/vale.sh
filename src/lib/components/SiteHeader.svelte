34bbc321962a015073c495c95c1aafb2c47ab151
Joseph Kato <joseph@jdkato.io>
2024-12-08 02:56:37 -0800

    Sheet -> Drawer

--- src/lib/components/SiteHeader.svelte
+++ src/lib/components/SiteHeader.svelte
@@ -12,7 +12,7 @@
 <header
 	class="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
 >
-	<Banner />
+	<!--<Banner />-->
 	<div class="container flex h-14 max-w-screen-2xl items-center">
 		<MainNav />
 		<MobileNav />
--- src/lib/components/nav/MobileNav.svelte
+++ src/lib/components/nav/MobileNav.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { Icons } from '../icons';
+	import { Icon } from 'svelte-icons-pack';
+	import { AiOutlineMenu } from 'svelte-icons-pack/ai';
 	import MobileLink from './MobileLink.svelte';
-	import * as Sheet from '$lib/components/ui/sheet';
+	import * as Drawer from '$lib/components/ui/drawer';
 	import { Button } from '$lib/components/ui/button';
 	import { ScrollArea } from '$lib/components/ui/scroll-area';
 	import { docsConfig } from '$lib/config/docs.js';
@@ -10,19 +12,19 @@
 	let open = false;
 </script>
 
-<Sheet.Root bind:open>
-	<Sheet.Trigger>
+<Drawer.Root bind:open>
+	<Drawer.Trigger>
 		<Button
 			variant="ghost"
 			class="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
 		>
-			<Icons.Hamburger class="h-5 w-5" />
+			<Icon src={AiOutlineMenu} className="h-5 w-5" />
 			<span class="sr-only">Toggle Menu</span>
 		</Button>
-	</Sheet.Trigger>
-	<Sheet.Content side="left" class="pr-0">
+	</Drawer.Trigger>
+	<Drawer.Content class="pr-0">
 		<MobileLink href="/" class="flex items-center" bind:open>
-			<Icons.logo class="mr-2 h-4 w-4" />
+			<Icons.logo class="mr-2 h-5 w-5" />
 			<span class="font-bold">{siteConfig.name}</span>
 		</MobileLink>
 		<ScrollArea orientation="both" class="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
@@ -59,5 +61,5 @@
 				{/each}
 			</div>
 		</ScrollArea>
-	</Sheet.Content>
-</Sheet.Root>
+	</Drawer.Content>
+</Drawer.Root>
