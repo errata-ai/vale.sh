83816d5efec04369919de69576499e670789d924
Joseph Kato <joseph@jdkato.io>
2026-09-06 14:11:29 -0700

    New Generator and Landing
    Signed-off-by: Joseph Kato <joseph@jdkato.io>

--- src/routes/generator/util.ts
+++
@@ -1,9 +0,0 @@
-export function clean(code: string) {
-    var spaced = code.replaceAll(/=/g, ' = ').replaceAll(/,/g, ', ');
-    var s1 = spaced.replace('StylesPath = styles', 'StylesPath = styles\n');
-    return s1.replace('Packages', '\nPackages');
-}
-
-export function isAlpha(str: string) {
-    return /^[a-zA-Z0-9]+$/.test(str);
-}
